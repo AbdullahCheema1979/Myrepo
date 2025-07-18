8ae2c5460c05aa08f5200a5154dbcf22336eac5e
Abdullah <abdullahcheema1979>
2025-07-18 22:26:39 +0500

    All

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main()
     {
         int a = 5, b = 3;
+        Console.WriteLine("Addition: " + (a + b));
+        Console.WriteLine("Subtraction: " + (a - b));
+        Console.WriteLine("Multiplication: " + (a * b));
         Console.WriteLine("Devision: " + (a/b));
     }
+    
 }
